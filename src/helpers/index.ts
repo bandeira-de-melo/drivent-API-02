9914c9a13511a1979ba8500ff43f5746d44ab0a0
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-07-07 15:31:13 -0300

    feat: hasUserTIcket helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -7,3 +7,11 @@ export async function isUserEnrolled(userId: number) {
     throw new Error('Internal Server Error');
   }
 }
+
+export async function hasUserTicket(enrollmentId: number) {
+  try {
+    return await prisma.ticket.findFirst({ where: { enrollmentId } });
+  } catch (error) {
+    throw new Error('Internal Server Error');
+  }
+}
